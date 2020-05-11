d00a8c38306a0c83ae6961bca799ea96cafc845c
Kris <kmringler@gmail.com>
2020-05-11 20:52:50 +0000

    still need to change regex for url destructuring

--- drills03/yourcode.js
+++ drills03/yourcode.js
@@ -2,25 +2,29 @@
 
 function getPath(path) {
   const newArray = [];
-  console.log(path.length)
   let newString = ""
   for (let i = 0; i < path.length + 1; i++) {
-
     if (path[i] === '/' || path[i] === path[path.length]) {
       newArray.push(newString);
       newString = ""
-
     } else {
       newString += path[i];
     }
-
   }
-
   return newArray;
 
 }
 
-function getPathParts() {
+function getPathParts(url) {
+  const parsedURL = /^(\w+)\:\/\/([^\/]+)\/(.*)$/.exec(url);
+  console.log('url: ', url)
+  console.log('parsedURL: ', parsedURL)
+  if (!parsedURL) {
+    return false;
+  }
+  const [, protocol, host, port, path, file] = parsedURL;
+
+  return { protocol, host, port, path, file }
 
 }
 
